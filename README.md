1219dfc7fea072d6e984fce5aad19b5d3334e170
AdamMoh <xlkartu12345@gmail.com>
2024-05-08 02:48:22 +0700

    Experiment 1.3: Multiple Spawn and removing drop.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 
  Since the code to print "Adam's Computer: hey hey" are outside of the asynchronous task, then it will printed first. After that the main code will run the asynchronous task and after printing the "Adam's Computer: howdy!" the program wait for 2 seconds because of `TimerFuture` set to wait for 2 seconds after that print the "Adam's Computer: done!"
 
+2. Experiment 1.3: Multiple Spawn and removing drop
+    With drop
+    <img src="assets/ss2.png">
+
+    No drop
+    <img src="assets/ss3.png">
+    So the function of `drop(spawner);` is to drop the spawner and it will not run the task anymore. So, the program will not wait for the task to finish and will print "Adam's Computer: done!" immediately.
--- src/main.rs
+++ src/main.rs
@@ -105,6 +105,20 @@ fn main() {
         println!("Adam's Computer: done!");
     });
 
+    spawner.spawn(async {
+        println!("Adam's Computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Adam's Computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Adam's Computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Adam's Computer: done3!");
+    });
+
     println!("Adam's Computer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
